89c609bc9cc5db26966bd926d7e8523f5b3aa35a
marcelo goncalves <tchelojg@gmail.com>
2020-12-23 00:00:05 -0300

    Restructuring project: testing and production environments.

---
+++ terraform/testing/main.tf
@@ -0,0 +1,18 @@
+resource "google_container_cluster" "primary" {
+  name = var.cluster_name
+  network = var.network
+  location = var.region
+  initial_node_count = var.initial_node_count
+}
+
+resource "google_container_node_pool" "primary_preemtible_nodes" {
+  name = var.node_name
+  location = var.region
+  cluster = google_container_cluster.primary.name
+  node_count = var.node_count
+
+  node_config {
+    preemptible = var.preemptible
+    machine_type = var.machine_type
+  }
+}
