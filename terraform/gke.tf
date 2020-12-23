2638bcd79d1385513c1ec55ef2b0ecd0f3666ddd
marcelo goncalves <tchelojg@gmail.com>
2020-12-22 23:58:32 -0300

    Reestructuring project: Adding production and testing folders

--- terraform/gke.tf
+++
@@ -1,6 +0,0 @@
-resource "google_container_cluster" "primary" {
-  name = "quotes-api"
-  network = "default"
-  location = "southamerica-east1"
-  initial_node_count = 1
-}
--- terraform/provider.tf
+++
@@ -1,5 +0,0 @@
-provider "google" {
-    credentials     = file("../auth/quotes-299423-3a622a0a01f1.json")
-    project         = var.project
-    region          = var.region
-}
