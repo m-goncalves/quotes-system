43af9d4a9e19284afaebe668f79924bdcd0039de
marcelo goncalves <tchelojg@gmail.com>
2021-01-07 16:26:31 -0300

    Adding resource for creation of a S3 bucket to store terraform state.

--- terraform/plan/main.tf
+++ terraform/plan/main.tf
@@ -1,24 +1,30 @@
 provider "google" {
-    credentials     = file(var.credentials)
-    project         = var.project
-    region          = var.region
+    credentials       = file(var.credentials)
+    project           = var.project
+    region            = var.region
 }
 
 resource "google_container_cluster" "primary" {
-  name = var.cluster_name
-  network = var.network
-  location = var.region
-  initial_node_count = var.initial_node_count
+  name                = var.cluster_name
+  network             = var.network
+  location            = var.region
+  initial_node_count  = var.initial_node_count
 }
 
 resource "google_container_node_pool" "primary_preemtible_nodes" {
-  name = var.node_name
-  location = var.region
-  cluster = google_container_cluster.primary.name
-  node_count = var.node_count
+  name                = var.node_name
+  location            = var.region
+  cluster             = google_container_cluster.primary.name
+  node_count          = var.node_count
 
   node_config {
-    preemptible = var.preemptible
-    machine_type = var.machine_type
+    preemptible       = var.preemptible
+    machine_type      = var.machine_type
   }
+}
+
+resource "google_storage_bucket" "state_bucket"{
+  name                = var.bucket_name 
+  location            = var.region
+  force_destroy       = true 
 }
